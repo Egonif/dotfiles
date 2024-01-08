f62b7524cba686b3e206a11381a4e4ace7a353c6
Egon <egon@mbp-de-egon.home>
2024-01-08 17:53:31 +0100

    Add powerlevel10k installation process

--- install.sh
+++ install.sh
@@ -33,3 +33,7 @@ fi
 
 # Kitty installation
 ./kitty/install.sh
+
+# Powerlevel10k installation
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
